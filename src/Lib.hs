a28f3d5f08dd237ba17bb2b16dc4bc5b867f005e
Wing Lee <44917547+wingleeio@users.noreply.github.com>
2024-10-29 12:50:18 -0700

    Add a utils file

--- src/Lib.hs
+++ src/Lib.hs
@@ -11,10 +11,8 @@ module Lib
   )
 where
 
-import qualified Data.ByteString.Char8 as BS
-import Data.Text (Text, pack, unpack)
+import Data.Text (Text)
 import qualified Data.Text.Encoding as TE
-import Data.Text.Read (decimal, signed)
 import qualified Lib.Templates as Templates
 import qualified Lib.State as AppState
 import Network.Wai
@@ -31,6 +29,7 @@ import Data.Function
 import Control.Lens
 import Data.OpenApi (ToSchema(declareNamedSchema))
 import Network.Wai.Logger (withStdoutLogger)
+import qualified Lib.Utils as Utils
 
 type API =
   Header "Cookie" Text :> Get '[HTML] H.Html
@@ -55,45 +54,37 @@ api :: Proxy API
 api = Proxy
 
 server :: ServerT API AppState.AppM
-server = homeApi :<|> incrementApi :<|> decrementApi :<|> Templates.scalar :<|> swaggerApi  :<|> serveDirectoryWebApp "public" 
-  where
-    homeApi = home
-    incrementApi = increment
-    decrementApi = decrement
-    swaggerApi = pure swagger
+server = home 
+  :<|> increment 
+  :<|> decrement 
+  :<|> Templates.scalar 
+  :<|> pure swagger  
+  :<|> serveDirectoryWebApp "public" 
 
-parseCount :: Maybe BS.ByteString -> Integer
-parseCount Nothing = 0
-parseCount (Just countStr) =
-  case signed decimal (TE.decodeUtf8 countStr) of
-    Right (n, _) -> n
-    Left _ -> 0
 
-makeCookie :: (Show a) => Text -> a -> Text
-makeCookie name value = pack $ unpack name ++ "=" ++ show value ++ "; Path=/"
 
 home :: Maybe Text -> AppState.AppM H.Html
 home Nothing = Templates.counter 0
 home (Just x) = do
   let cookies = parseCookies (TE.encodeUtf8 x)
-  let n = parseCount (lookup "haskell.count" cookies)
+  let n = Utils.parseCount (lookup "haskell.count" cookies)
   Templates.counter n
 
 increment :: Maybe Text -> AppState.AppM(Headers '[Header "Set-Cookie" Text] H.Html)
 increment Nothing = pure $ addHeader "haskell.count=1; Path=/" (Templates.count 1)
 increment (Just x) = do
   let cookies = parseCookies (TE.encodeUtf8 x)
-  let n = parseCount (lookup "haskell.count" cookies)
+  let n = Utils.parseCount (lookup "haskell.count" cookies)
   let newCount = n + 1
-  pure $ addHeader (makeCookie "haskell.count" newCount) (Templates.count newCount)
+  pure $ addHeader (Utils.makeCookie "haskell.count" newCount) (Templates.count newCount)
 
 decrement :: Maybe Text -> AppState.AppM(Headers '[Header "Set-Cookie" Text] H.Html)
 decrement Nothing = pure $ addHeader "haskell.count=-1; Path=/" (Templates.count (-1))
 decrement (Just x) = do
   let cookies = parseCookies (TE.encodeUtf8 x)
-  let n = parseCount (lookup "haskell.count" cookies)
+  let n = Utils.parseCount (lookup "haskell.count" cookies)
   let newCount = n - 1
-  pure $ addHeader (makeCookie "haskell.count" newCount) (Templates.count newCount)
+  pure $ addHeader (Utils.makeCookie "haskell.count" newCount) (Templates.count newCount)
 
 swagger :: OA.OpenApi
 swagger = SOA.toOpenApi api
@@ -103,8 +94,7 @@ swagger = SOA.toOpenApi api
   & OA.info.OA.license ?~ ("MIT" & OA.url ?~ OA.URL "https://haskell-htmx.uwulabs.io")
 
 instance OA.ToSchema H.Html where
-    declareNamedSchema _ = do
-        pure $ OA.NamedSchema Nothing $ mempty 
+    declareNamedSchema _ = pure $ OA.NamedSchema Nothing $ mempty 
             & OA.type_ ?~ OA.OpenApiString
             & OA.description ?~ "HTML content"
             & OA.format ?~ "html"
---
+++ src/Lib/Utils.hs
@@ -0,0 +1,16 @@
+module Lib.Utils (parseCount, makeCookie)where
+
+import qualified Data.ByteString as BS
+import Data.Text ( pack, unpack, Text )
+import qualified Data.Text.Encoding as TE
+import Data.Text.Read (decimal, signed)
+
+parseCount :: Maybe BS.ByteString -> Integer
+parseCount Nothing = 0
+parseCount (Just countStr) =
+  case signed decimal (TE.decodeUtf8 countStr) of
+    Right (n, _) -> n
+    Left _ -> 0
+
+makeCookie :: (Show a) => Text -> a -> Text
+makeCookie name value = pack $ unpack name ++ "=" ++ show value ++ "; Path=/"
